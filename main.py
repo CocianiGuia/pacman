d250053316920ca2a634c5835f6236da5ac4d910
BeaaColoM <beatrice.colombimoroni@gmail.com>
2024-05-19 23:25:09 +0200

    finestragioco e nome

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame, sys
 from pygame.locals import *
 
-pygame.display.set_caption('PacMan')
+
+
+window_size=(900,600)
+screen=pygame.display.set_mode(window_size,0,32)
+display=pygame.Surface((900,600))
+
+pygame.display.set_caption('Pac-Man')
 
 clock= pygame.time.Clock()
+fps=60
