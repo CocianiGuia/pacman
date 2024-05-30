c3f5fd33a2ad5779d669db19ff9707ff4286d75c
BeaaColoM <beatrice.colombimoroni@gmail.com>
2024-05-30 23:21:30 +0200

    punteggio

---
+++ punteggio.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class Punti:
+    def __init__(self, screen, pos, size) -> None:
+        self.screen = screen
+        self.pos = pos
+        self.size = size
+
+        self.punti = 0
+
+        self.image = pygame.Surface(size)
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
+
+    def draw(self):
+        self.image.fill(BLACK)
+
+        # l'altezza disponibile è 60
+        font = pygame.font.Font(None, 65)
+        text = font.render(str(self.punti), 1, WHITE)
+        self.image.blit(text, (250, 10)) # surface e pos, quando passo un rect di esso viene comunque presa solo la posizione
+
+        self.screen.blit(self.image, self.rect)
