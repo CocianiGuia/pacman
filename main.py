1ddbfe76e2f18fa936c76a36daec6df6f1dc2266
CocianiGuia <cociani07@gmail.com>
2024-05-31 09:29:41 +0200

    menu(movimento(esc))

--- main.py
+++ main.py
@@ -66,47 +66,56 @@ menu=pygame.image.load("immaginimenù/menu.png")
 
 #         draw_text('START', font, BLACK, screen, 1100//2, )
 
-inizio=True
+
 # def gioca(): 
+def menu(screen):
+        screen.fill("black")
+        icona_bottone=icona.get_rect(center=(350, 300))
+        font=pygame.font.Font(None, 70)
+        gioca=font.render("START", True, "white", None)
+        rectbot=gioca.get_rect(center=(350,550))
+        clock=pygame.time.Clock()
+        fps=60
+        b=True
+        while b==True:
+            lista=pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type==MOUSEBUTTONDOWN and event.button==1:
+                    if rectbot.collidepoint(lista):
+                        b=False
+            screen.blit(icona, icona_bottone)
+            screen.blit(gioca,rectbot)
+            clock.tick(fps)
+            pygame.display.flip()
+menu(screen)
 while True:
-
-    if inizio:
-
-        screen.blit(menu, (0,0))
-        screen.blit(icona, (100,100))
-        icona_bottone=icona.get_rect(topleft=(170,230))
-        pygame.display.update()
-        clock.tick(fps)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type==MOUSEBUTTONDOWN:
-                if icona_bottone.collidepoint(pygame.mouse.get_pos()):
-                    print("s")
-                    inizio=False
-
-        
-
-    else:
-
-        keys = pygame.key.get_pressed()
-        if keys[K_RIGHT]: 
-            pacman.move_right()
-        if keys[K_LEFT]:
-            pacman.move_left()
-        if keys[K_UP]:
-            pacman.move_up()
-        if keys[K_DOWN]:
-            pacman.move_down()
-        pacman.move()
-        clock.tick(fps)   
-        #screen.fill("BLACK")#per pulire lo schermo e non fare la scia 
-        labirinto.draw()
-        punti.draw()
-        # ciliegia.sceglirettangolo(pacman.rect)
-        pacman.draw(pygame.time.get_ticks())
-        # ciliegia.draw()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==K_ESCAPE:
+                menu(screen)
+    keys = pygame.key.get_pressed()
+    if keys[K_RIGHT]: 
+        pacman.move_right()
+    if keys[K_LEFT]:
+        pacman.move_left()
+    if keys[K_UP]:
+        pacman.move_up()
+    if keys[K_DOWN]:
+        pacman.move_down()
+    pacman.move()
+    clock.tick(fps)   
+    #screen.fill("BLACK")#per pulire lo schermo e non fare la scia 
+    labirinto.draw()
+    punti.draw()
+    # ciliegia.sceglirettangolo(pacman.rect)
+    pacman.draw(pygame.time.get_ticks())
+    # ciliegia.draw()
         
     pygame.display.update()
     clock.tick(fps)
